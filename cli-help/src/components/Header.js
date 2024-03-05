54e855408bb4faabe34ca4e461aaa4b53f163b5f
Admin LFMS <luizfms8585@gmail.com>
2024-03-05 01:07:44 -0300

    Header Started

---
+++ cli-help/src/components/Header.js
@@ -0,0 +1,22 @@
+const Heading = () =>  {
+
+return(
+
+<>
+<div className="Header">
+        <div className="Logo">CH</div>
+        <div className="nav-menu">
+            <span className="menu-options home">home</span>
+            <span className="menu-options prompts">prompts</span>
+            <span className="menu-options categories">categories</span>
+        </div>
+    </div>
+</>
+
+)
+
+
+}
+
+
+export default Heading;
---
+++ cli-help/src/pages/_app.js
@@ -0,0 +1,20 @@
+import "../styles/globals.css"
+import Heading from "../components/Header.js"
+
+
+const App = () => {
+
+    return(
+
+        <>
+        
+        <Heading />
+        
+        </>
+
+
+    )
+
+    
+}
+export default App;
